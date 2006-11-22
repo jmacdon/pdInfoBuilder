de0c71379b5177f99bc5884636b9edeb0e9432fc
sethf <sethf@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-11-22 23:55:34 +0000

    Add package building capability to pdInfoBuilder
    Move accessor functions for PDInfo objects to AnnotationDbi
    (this may not be the permanent home).
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pdInfoBuilder@21255 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/makeDataPackage-methods.R
@@ -0,0 +1,48 @@
+setClass("PDInfoPkg",
+         representation=representation(
+           chipName="character",
+           manufacturer="character",
+           pdInfoObjectName="character"))
+
+setClass("PDInfoAffySNPPkg",
+         contains="PDInfoPkg",
+         representation=representation(
+           cdfFile="character",
+           csvAnnoFile="character",
+           csvSeqFile="character"
+           ))
+
+##pkg <- new("PDInfoAffySNPPkg", chipName="Mapping250nsp", manufacturer="Affymetrix", pdInfoObjectName="map250nsp", cdfFile="", csvAnnoFile="", csvSeqFile="")
+##makeDataPackage(pkg, "Seth Falcon", "sfalcon@fhcrc.org", "pd.Map250nsp", "0.0.7", biocViews="AnnotationData", filePath=".")
+
+setMethod(Biobase::makeDataPackage,
+          signature(object="PDInfoAffySNPPkg"),
+          function(object, author, email, packageName, packageVersion, license, biocViews,
+                   filePath, quiet=FALSE) {
+
+              if (missing(email) || !is.character(email)
+                  || length(email) != 1 || grep("@", email) != 1)
+                stop("invalid email address")
+
+              extdataDir <- file.path(filePath, packageName, "inst", "extdata")
+              dbFile <- file.path(extdataDir, paste(packageName, "sqlite", sep="."))
+              syms <- list(MANUF=object@manufacturer,
+                           VERSION=packageVersion,
+                           AUTHOR=author,
+                           AUTHOREMAIL=email,
+                           LIC=license,
+                           DBFILE=dbFile,
+                           CHIPNAME=object@chipName,
+                           PKGNAME=packageName,
+                           PDINFONAME=object@pdInfoObjectName,
+                           PDINFOCLASS="SNPPDInfo")
+
+              templateDir <- system.file("pd.PKG.template",
+                                         package="pdInfoBuilder")
+              createPackage(pkgname=packageName, destinationDir=filePath,
+                            originDir=templateDir,
+                            symbolValues=syms, quiet=quiet)
+              dir.create(extdataDir, recursive=TRUE)
+              buildPdInfoDb(object@cdfFile, object@csvAnnoFile,
+                            object@csvSeqFile, dbFile)
+          })
---
+++ inst/pd.PKG.template/R/all.R
@@ -0,0 +1,53 @@
+globals <- new.env(hash=TRUE, parent=emptyenv())
+
+globals$DEBUG <- FALSE
+
+globals$DB_PATH <- system.file("extdata", "@DBFILE@",
+                               package="@PKGNAME@")
+if (nchar(globals$DB_PATH) == 0)
+  stop("Unable to locate DB file")
+
+initDbConnection <- function() {
+    globals$dbCon <- dbConnect(dbDriver("SQLite"), dbname=globals$DB_PATH)
+    globals$dbCon
+}
+
+
+getDb  <- function() {
+    if (!is.null(globals$dbCon))
+      return(globals$dbCon)
+    initDbConnection()
+}
+
+
+closeDb <- function() {
+    ## FIXME: check for valid connection?
+    dbDisconnect(globals$dbCon)
+    remove(dbCon, envir=globals)
+}
+
+
+
+.onLoad <- function(libname, pkgname) {
+    require("methods", quietly=TRUE)
+    ## Establish a connection to the SQLite DB
+    initDbConnection()
+}
+
+.onAttach <- function(libname, pkgname) {
+    ## FIXME: once we've made a more careful pass over the data, this message
+    ## should go away!
+    cat("\n\n")
+    cat("Welcome to the pd.@CHIPNAME@ prototype pdInfo package\n")
+    cat("WARNING: DO NOT USE THIS PACKAGE FOR ANY ANALYSIS.\n")
+    cat("THIS PACKAGE IS FOR INTERFACE PROTOTYPE USE ONLY!\n")
+    cat("THE DATA HAS NOT BEEN VALIDATED AND LIKELY HAS ERRORS.\n")
+    cat("Have fun!\n\n")
+}
+
+.onUnload <- function(libpath) {
+    closeDb()
+}
+
+@PDINFONAME@ <- new("@PDINFOCLASS@", get_db=getDb)
+
