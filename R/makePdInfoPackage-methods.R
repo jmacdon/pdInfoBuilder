fae35901212a4540005f842aff956ce74f00ff67
sethf <sethf@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-12-20 04:35:39 +0000

    Refactor PDInfo Package Creation Code
    Create our own generic, makePdInfoPackage, for package
    creation instead of reusing makeDataPackage from Biobase.
    
    Expand the package "seed" class to contain all needed package meta
    data.
    
    Add code to extract chip name from the CDF file and use
    it to set the package name.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pdInfoBuilder@21594 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/makePdInfoPackage-methods.R
@@ -0,0 +1,86 @@
+setClass("PkgSeed",
+         representation=representation(
+           name="character",
+           version="character",
+           license="character",
+           author="character",
+           email="character",
+           url="character",
+           biocViews="character"),
+         prototype=prototype(
+           license="Artistic License, Version 2.0"),
+         validity=function(object) .isValidPkgSeed(object))
+
+setClass("PDInfoPkgSeed",
+         contains="PkgSeed",
+         representation=representation(
+           chipName="character",
+           manufacturer="character",
+           genomebuild="character",
+           pdInfoObjectName="character"))
+
+setClass("AffySNPPDInfoPkgSeed",
+         contains="PDInfoPkgSeed",
+         representation=representation(
+           cdfFile="character",
+           csvAnnoFile="character",
+           csvSeqFile="character"
+           ),
+         prototype=list(manufacturer="Affymetrix"))
+
+## FIXME: we should offer to determine packageName based on header of
+## CDF file and oligo::cleanPlatformName.
+
+setGeneric("chipName", function(object) standardGeneric("chipName"))
+
+setMethod("chipName", "AffySNPPDInfoPkgSeed",
+          function(object) {
+              ## compute chip name from the CDF file
+              header <- readCdfHeader(object@cdfFile)
+              header$chiptype
+          })
+
+
+setGeneric("makePdInfoPackage", signature=c("object"),
+           function(object, destDir, quiet=FALSE) {
+               standardGeneric("makePdInfoPackage")
+           })
+
+
+setMethod("makePdInfoPackage", "AffySNPPDInfoPkgSeed",
+          function(object, destDir=".", quiet=FALSE) {
+              chip <- chipName(object)
+              pkgName <- cleanPlatformName(chip)
+              extdataDir <- file.path(destDir, pkgName, "inst", "extdata")
+              dbFileName <- paste(pkgName, "sqlite", sep=".")
+              dbFilePath <- file.path(extdataDir, dbFileName)
+              seqMatFile <- file.path(extdataDir, "seqMat.rda")
+              syms <- list(MANUF=object@manufacturer,
+                           VERSION=object@version,
+                           GENOMEBUILD=object@genomebuild,
+                           AUTHOR=object@author,
+                           AUTHOREMAIL=object@email,
+                           LIC=object@license,
+                           DBFILE=dbFileName,
+                           CHIPNAME=chip,
+                           PKGNAME=pkgName,
+                           PDINFONAME=pkgName,
+                           PDINFOCLASS="AffySNPPDInfo")
+
+              templateDir <- system.file("pd.PKG.template",
+                                         package="pdInfoBuilder")
+              createPackage(pkgname=pkgName, destinationDir=destDir,
+                            originDir=templateDir, symbolValues=syms,
+                            quiet=quiet)
+              dir.create(extdataDir, recursive=TRUE)
+              buildPdInfoDb(object@cdfFile, object@csvAnnoFile,
+                            object@csvSeqFile, dbFilePath, seqMatFile)
+          })
+
+
+.isValidPkgSeed <- function(object) {
+    email <- object@email
+    if (length(email) != 1 || grep("@", email) != 1)
+      return("invalid email address")
+    TRUE
+}
