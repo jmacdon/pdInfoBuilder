42ce17870edd23ef9c4ec4030299f6da5855fb47
stvjc <stvjc@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-06-24 16:17:43 +0000

    added geometry testing into checkTables
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pdInfoBuilder@25533 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/pd.PKG.template/inst/UnitTests/checkTables_test.R
+++ inst/pd.PKG.template/inst/UnitTests/checkTables_test.R
@@ -27,3 +27,25 @@ test_table_info <- function() {
     checkEquals(nrfs, nrow(fstab))
 }
 
+test_table_info <- function() {
+    ##
+    ## this test verifies that the table_info table knows the correct
+    ## number of rows for featureSet -- commented out a table extraction
+    ##
+    dd  <- pdinfo@getdb()
+    tinfo  <- dbGetQuery(dd, "select * from table_info")
+    nrfs  <- as.numeric(tinfo[ tinfo$tbl == "featureSet", "row_count" ])
+#    fstab  <- dbGetQuery(dd, "select * from featureSet")
+    nrc  <- dbGetQuery(dd, "select count(*) from featureSet")[[1]]
+    checkEquals(nrfs, as.numeric(nrc))
+}
+
+test_geom <- function() {
+    dd <- pdinfo@getdb()
+    xylim = geometry(pdinfo)
+    xs = dbGetQuery(dd, "select x from pmfeature")[[1]]
+    ys = dbGetQuery(dd, "select y from pmfeature")[[1]]
+    checkTrue(max(xs) <= xylim[1]) # zero offset?
+    checkTrue(max(ys) <= xylim[2]) # zero offset?
+}
+
