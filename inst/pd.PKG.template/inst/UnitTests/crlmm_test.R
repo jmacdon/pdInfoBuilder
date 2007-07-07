9b2d0074054c4103659fc61894340d38a0300443
stvjc <stvjc@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-07-07 13:58:43 +0000

    crlmm_test first install
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pdInfoBuilder@25690 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/pd.PKG.template/inst/UnitTests/crlmm_test.R
@@ -0,0 +1,21 @@
+pdinfo <- @PDINFONAME@
+
+# the command
+# eg. library(pd.mapping50k.hind240)
+# will have been run in the harness
+
+test_crlmm <- function() {
+# this stuff will work for the 4 chips we know about now
+# 100k hind240, xba240; 250k sty, nsp
+size = as.character(2*as.numeric(gsub(".*g(.*)k.*", "\\1", pdinfo)))
+enz = gsub(".*k.(.*$)", "\\1", pdinfo)
+enz = gsub("240", "", enz)
+hapmPackName = paste("hapmap", size, enz, sep="")
+library(oligo)
+library(hapmPackName, character.only=TRUE)
+xxr = justSNPRMA(dir(system.file( "celFiles", package=hapmPackName), full=TRUE))
+xxc = crlmm(xxr, correctionFile="corr.rda")
+if (exists("xxc")) return(TRUE)
+return(FALSE)
+}
+
