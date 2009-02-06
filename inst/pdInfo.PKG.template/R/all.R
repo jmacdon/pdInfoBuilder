0dba0eef9ffc1bb1eaf4603bdcb381d762fbd158
m.settles <m.settles@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-02-06 19:52:36 +0000

    fixed template build error
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pdInfoBuilder@37216 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/pdInfo.PKG.template/R/all.R
@@ -0,0 +1,52 @@
+globals <- new.env(hash=TRUE, parent=emptyenv())
+
+globals$DEBUG <- FALSE
+
+## setup the path at package level so that DB can be accessed
+## during package install/lazyload db creation.
+##
+## We reset the DB_PATH in .onLoad since we need to
+## get the right one based on libpath
+globals$DB_PATH <- system.file("extdata", "@DBFILE@",
+                               package="@PKGNAME@")
+if (nchar(globals$DB_PATH) == 0)
+  stop("Unable to locate DB file")
+
+initDbConnection <- function() {
+    globals$dbCon <- dbConnect(dbDriver("SQLite"), dbname=globals$DB_PATH)
+    globals$dbCon
+}
+
+getDb  <- function() {
+    if (!is.null(globals$dbCon) && isIdCurrent(globals$dbCon))
+      return(globals$dbCon)
+    initDbConnection()
+}
+
+closeDb <- function() {
+    ## FIXME: check for valid connection?
+    sapply(dbListResults(globals$dbCon), dbClearResult)
+    dbDisconnect(globals$dbCon)
+    remove(dbCon, envir=globals)
+}
+
+.onLoad <- function(libname, pkgname) {
+    require("methods", quietly=TRUE)
+    globals$DB_PATH <- system.file("extdata", "@DBFILE@",
+                                   package="@PKGNAME@",
+                                   lib.loc=libname)
+    if (nchar(globals$DB_PATH) == 0)
+      stop("Unable to locate DB file")
+    ## Establish a connection to the SQLite DB
+    initDbConnection()
+}
+
+.onUnload <- function(libpath) {
+    closeDb()
+}
+
+@PDINFONAME@ <- new("@PDINFOCLASS@",
+                    genomebuild="@GENOMEBUILD@",
+                    getdb=getDb,
+                    geometry=as.integer(strsplit("@GEOMETRY@", ";")[[1]]))
+
