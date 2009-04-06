3426666a9acc2915345c4beb16a8a7e48ebd88ea
p.aboyoun <p.aboyoun@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-04-06 22:16:22 +0000

    Added some dontrun tags around two man page examples since they show how to interact with non-existant files.
    Also, cleaned up the initialize methods a little to hopefully avoid problems when instanciating objects.
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pdInfoBuilder@38528 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/initialize-methods.R
+++ R/initialize-methods.R
@@ -1,43 +1,55 @@
 ############## Affymetrix Arrays ###############
 setMethod("initialize", "AffyExpressionPDInfoPkgSeed",
           function(.Object, cdfFile, celFile, tabSeqFile, ...) {
-              .Object@cdfFile <- new("fileName", cdfFile)
-              .Object@celFile <- new("fileName", celFile)
-              .Object@tabSeqFile <- new("fileName", tabSeqFile)
-              .Object <- callNextMethod(.Object, ...)
-              .Object
+              if (!missing(cdfFile))
+                .Object@cdfFile <- new("fileName", cdfFile)
+              if (!missing(celFile))
+                .Object@celFile <- new("fileName", celFile)
+              if (!missing(tabSeqFile))
+                .Object@tabSeqFile <- new("fileName", tabSeqFile)
+              callNextMethod(.Object, ...)
           })
 
 setMethod("initialize", "AffySNPPDInfoPkgSeed2",
           function(.Object, cdfFile, csvAnnoFile, csvSeqFile, ...){
-            message("I'm in 'initialize' for AffySNPPDInfoPkgSeed2... ")
-            .Object@cdfFile <- new("fileName", cdfFile)
-            .Object@csvAnnoFile <- new("fileName", csvAnnoFile)
-            .Object@csvSeqFile <- new("fileName", csvSeqFile)
-            callNextMethod()
+            if (!missing(cdfFile))
+              .Object@cdfFile <- new("fileName", cdfFile)
+            if (!missing(csvAnnoFile))
+              .Object@csvAnnoFile <- new("fileName", csvAnnoFile)
+            if (!missing(csvSeqFile))
+              .Object@csvSeqFile <- new("fileName", csvSeqFile)
+            callNextMethod(.Object, ...)
           })
 
 setMethod("initialize", "AffySNPPDInfoPkgSeed",
           function(.Object, splineParamFile, crlmmInfoFile, referenceDistFile, ...){
-            .Object@splineParamFile <- new("fileName", splineParamFile)
-            .Object@crlmmInfoFile <- new("fileName", crlmmInfoFile)
-            .Object@referenceDistFile <- new("fileName", referenceDistFile)
-            callNextMethod()
+            if (!missing(splineParamFile))
+              .Object@splineParamFile <- new("fileName", splineParamFile)
+            if (!missing(crlmmInfoFile))
+              .Object@crlmmInfoFile <- new("fileName", crlmmInfoFile)
+            if (!missing(referenceDistFile))
+              .Object@referenceDistFile <- new("fileName", referenceDistFile)
+            callNextMethod(.Object, ...)
           })
 
 setMethod("initialize", "AffySNPCNVPDInfoPkgSeed2",
           function(.Object, csvAnnoFileCnv, csvSeqFileCnv, ...){
-            .Object@csvAnnoFileCnv <- new("fileName", csvAnnoFileCnv)
-            .Object@csvSeqFileCnv <- new("fileName", csvSeqFileCnv)
-            callNextMethod()
+            if (!missing(csvAnnoFileCnv))
+              .Object@csvAnnoFileCnv <- new("fileName", csvAnnoFileCnv)
+            if (!missing(csvSeqFileCnv))
+              .Object@csvSeqFileCnv <- new("fileName", csvSeqFileCnv)
+            callNextMethod(.Object, ...)
           })
 
 setMethod("initialize", "AffySNPCNVPDInfoPkgSeed",
           function(.Object, splineParamFile, crlmmInfoFile, referenceDistFile, ...){
-            .Object@splineParamFile <- new("fileName", splineParamFile)
-            .Object@crlmmInfoFile <- new("fileName", crlmmInfoFile)
-            .Object@referenceDistFile <- new("fileName", referenceDistFile)
-            callNextMethod()
+            if (!missing(splineParamFile))
+              .Object@splineParamFile <- new("fileName", splineParamFile)
+            if (!missing(crlmmInfoFile))
+              .Object@crlmmInfoFile <- new("fileName", crlmmInfoFile)
+            if (!missing(referenceDistFile))
+              .Object@referenceDistFile <- new("fileName", referenceDistFile)
+            callNextMethod(.Object, ...)
           })
 
 #### setMethod("initialize", "AffySNPPDInfoPkgSeed",
@@ -72,42 +84,49 @@ setMethod("initialize", "AffySNPCNVPDInfoPkgSeed",
   
 setMethod("initialize", "AffyTilingPDInfoPkgSeed",
           function(.Object, bpmapFile, celFile, ...) {
-              .Object@bpmapFile <- new("fileName", bpmapFile)
-              .Object@celFile <- new("fileName", celFile)
-              .Object <- callNextMethod(.Object, ...)
-              .Object
+              if (!missing(bpmapFile))
+                .Object@bpmapFile <- new("fileName", bpmapFile)
+              if (!missing(celFile))
+                .Object@celFile <- new("fileName", celFile)
+              callNextMethod(.Object, ...)
           })
   
 setMethod("initialize", "AffySTPDInfoPkgSeed",
           function(.Object, pgfFile, clfFile, probeFile, transFile, ...) {
-              .Object@pgfFile <- new("fileName", pgfFile)
-              .Object@clfFile <- new("fileName", clfFile)
+              if (!missing(pgfFile))
+                .Object@pgfFile <- new("fileName", pgfFile)
+              if (!missing(clfFile))
+                .Object@clfFile <- new("fileName", clfFile)
               ## probeFile and transFile are optional
               if (!missing(probeFile))
-                  .Object@probeFile <- new("fileName", probeFile)
+                .Object@probeFile <- new("fileName", probeFile)
               if (!missing(transFile))
-                  .Object@transFile <- new("fileName", transFile)
-              .Object <- callNextMethod(.Object, ...)
-              .Object
+                .Object@transFile <- new("fileName", transFile)
+              callNextMethod(.Object, ...)
           })
   
 
 ########### Nimblegen Arrays ###############
 setMethod("initialize", "NgsExpressionPDInfoPkgSeed",
           function(.Object, ndfFile, xysFile, pairFile, ngdFile, ...) {
-            .Object@ndfFile <- new("fileName", ndfFile)
+            if (!missing(ndfFile))
+              .Object@ndfFile <- new("fileName", ndfFile)
             if (!missing(xysFile))
               .Object@xysFile <- new("fileName", xysFile)
+            if (!missing(pairFile))
+              .Object@pairFile <- new("fileName", pairFile)
+            if (!missing(ngdFile))
+              .Object@ngdFile <- new("fileName", ngdFile)
             callNextMethod(.Object, ...)
-            .Object <- callNextMethod(.Object, ...)
-            .Object
         })
 
 setMethod("initialize", "NgsTilingPDInfoPkgSeed",
           function(.Object, ndfFile, xysFile, posFile, ...){
-            .Object@ndfFile <- new("fileName", ndfFile)
-            .Object@xysFile <- new("fileName", xysFile)
-            .Object@posFile <- new("fileName", posFile)
-            .Object <- callNextMethod(.Object, ...)
-            .Object
+            if (!missing(ndfFile))
+              .Object@ndfFile <- new("fileName", ndfFile)
+            if (!missing(xysFile))
+              .Object@xysFile <- new("fileName", xysFile)
+            if (!missing(posFile))
+              .Object@posFile <- new("fileName", posFile)
+            callNextMethod(.Object, ...)
           })
